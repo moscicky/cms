471e4cfaf1cef3d079654764fb5f8ddf2476b5da
maciejmoscicki <moscicki.maciej619@gmail.com>
2021-01-19 19:07:49 +0100

    Initial commit

---
+++ backend/src/main/kotlin/pl/moscicki/cms/CmsApplication.kt
@@ -0,0 +1,11 @@
+package pl.moscicki.cms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CmsApplication
+
+fun main(args: Array<String>) {
+    runApplication<CmsApplication>(*args)
+}
